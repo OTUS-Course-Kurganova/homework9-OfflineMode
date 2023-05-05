b2d1172005c8d5d310302b3c9ad14148edad70d4
Alexandra Kurganova <axndkgn@icloud.com>
2023-05-05 16:16:44 +0300

    Initial Commit

---
+++ hwCoreML/hwCoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  hwCoreMLApp.swift
+//  hwCoreML
+//
+//  Created by Alexandra Kurganova on 05.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct hwCoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
